54ce69bc95f55fd4246ef3631512530e0ab2e145
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-05-21 08:57:08 +0300

    Add menu

--- Kazka2/app/src/main/java/media/suspilne/kazky/MainActivity.java
+++ Kazka2/app/src/main/java/media/suspilne/kazky/MainActivity.java
@@ -3,13 +3,17 @@ package media.suspilne.kazky;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        onNavigationItemSelected(navigationView.getMenu().findItem(R.id.radio_menu));
     }
 
     @Override
@@ -48,6 +56,8 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        Toast.makeText(this, "onOptionsItemSelected: " + id, Toast.LENGTH_LONG).show();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -56,43 +66,70 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void exit(){
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //'No' button clicked
+                    break;
+            }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Вийти з Казок?")
+                .setPositiveButton("Так", dialogClickListener)
+                .setNegativeButton("Ні", dialogClickListener)
+                .show();
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
 
-        if (id == R.id.radio_menu) {
+        switch (item.getItemId()) {
+            case R.id.radio_menu:
+//                fragment = new RadioFragment();
+                title  = item.getTitle().toString();
+                break;
 
-        } else if (id == R.id.tales_menu) {
+            case R.id.tales_menu:
+//                fragment = new TalesFragment();
+                title  = item.getTitle().toString();
+                break;
 
-        } else if (id == R.id.settings_menu) {
+            case R.id.settings_menu:
+//                fragment = new SettingsFragment();
+                title  = item.getTitle().toString();
+                break;
 
-        } else if (id == R.id.exit_menu) {
-            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    switch (which){
-                        case DialogInterface.BUTTON_POSITIVE:
-                            android.os.Process.killProcess(android.os.Process.myPid());
-                            System.exit(1);
-                            break;
-
-                        case DialogInterface.BUTTON_NEGATIVE:
-                            //No button clicked
-                            break;
-                    }
-                }
-            };
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("Вийти з Казок?")
-                .setPositiveButton("Так", dialogClickListener)
-                .setNegativeButton("Ні", dialogClickListener)
-                    .show();
+            case R.id.exit_menu:
+                exit();
+                break;
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(fragment, "some text: " + title);
+//            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+
+        ((DrawerLayout) findViewById(R.id.drawer_layout)).closeDrawer(GravityCompat.START);
         return true;
     }
 }
