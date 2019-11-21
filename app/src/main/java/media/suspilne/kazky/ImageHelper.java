38e23caae305269da5b35bfd448650eb48d9db8b
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-11-21 10:02:47 +0200

    Adjust 'save' and 'star' background icons

--- app/src/main/java/media/suspilne/kazky/ImageHelper.java
+++ app/src/main/java/media/suspilne/kazky/ImageHelper.java
@@ -61,15 +61,12 @@ public class ImageHelper {
 
     public static Bitmap getBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
 
-        // First decode with inJustDecodeBounds=true to check dimensions
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
+
         BitmapFactory.decodeResource(res, resId, options);
 
-        // Calculate inSampleSize
         options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeResource(res, resId, options);
     }
