c7ae47aafc8084568e75d02ddc7096ef55c1fad5
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2020-09-08 10:49:06 +0300

    Fix crash on Android8. Ask/Check write permissions to save files.

--- app/src/main/java/media/suspilne/kazky/ActivityColorings.java
+++ app/src/main/java/media/suspilne/kazky/ActivityColorings.java
@@ -1,6 +1,8 @@
 package media.suspilne.kazky;
 
+import android.Manifest;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -8,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class ActivityColorings extends ActivityMain {
     private LinearLayout TalesList;
@@ -19,6 +24,10 @@ public class ActivityColorings extends ActivityMain {
 
         TalesList = findViewById(R.id.talesList);
         showTales();
+
+        if (ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
+        }
     }
 
     private void showTales(){
@@ -39,17 +48,21 @@ public class ActivityColorings extends ActivityMain {
                     return;
                 }
 
+                if (ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), R.string.no_write_permission, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Toast.makeText(getActivity(), R.string.coloring_download, Toast.LENGTH_LONG).show();
+
                 String url = ActivityColorings.getActivity().getResources().getString(R.string.coloringUrl, tale.coloring);
-                String fileName = String.format("kazka-%d.pdf", tale.coloring);
 
                 DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "kazka.pdf");
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
                 request.allowScanningByMediaScanner();
                 DownloadManager manager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
                 manager.enqueue(request);
-
-                Toast.makeText(getActivity(), R.string.coloring_download ,Toast.LENGTH_LONG).show();
             });
         }
 
