4a0b4aa7ccca7ff898aef3cd26bc9bd594544321
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2021-01-07 19:19:11 +0200

    Filter colorings by title/reader

--- app/src/main/java/media/suspilne/kazky/ActivityColorings.java
+++ app/src/main/java/media/suspilne/kazky/ActivityColorings.java
@@ -1,14 +1,23 @@
 package media.suspilne.kazky;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.DownloadManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -17,25 +26,58 @@ import androidx.core.content.ContextCompat;
 
 public class ActivityColorings extends ActivityMain {
     private LinearLayout TalesList;
+    private EditText searchField;
+    private ImageView searchIcon;
+    private Tales tales;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         currentView = R.id.coloring_menu;
         super.onCreate(savedInstanceState);
 
+        tales  = new Tales();
         TalesList = findViewById(R.id.talesList);
-        showTales();
+        searchField = findViewById(R.id.searchField);
+        searchIcon = findViewById(R.id.searchIcon);
 
-        if (ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
+        findViewById(R.id.showFavorite).setVisibility(View.GONE);
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMarginEnd(0);
+        searchIcon.setLayoutParams(lp);
+
+        addSearchField();
+        showTales();
+        filterTales();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
         }
     }
 
-    private void showTales(){
-        boolean showBigImages = SettingsHelper.getBoolean("showBigImages");
-        Tales tales = new Tales();
+    private void filterTales() {
+        activityTitle.setText(Tales.getFilter().equals("") ? getString(R.string.coloring) : "\u2315 " + Tales.getFilter());
+        TextView nothing = findViewById(R.id.nothingToShow);
+        int visibility = View.VISIBLE;
+        String filter = searchField.getText().toString();
 
-        for (final Tale tale:tales.items) {
+        for (final Tale tale : tales.getTalesList()) {
+            if (tale.matchesFilter(filter)) {
+                tale.show();
+                visibility = View.GONE;
+            } else {
+                tale.hide();
+            }
+        }
+
+        nothing.setVisibility(visibility);
+        nothing.setText(R.string.nothing);
+    }
+
+    private void showTales() {
+        boolean showBigImages = SettingsHelper.getBoolean("showBigImages");
+
+        for (final Tale tale : tales.items) {
             if (tale.coloring == 0) continue;
 
             View taleView = LayoutInflater.from(this).inflate(showBigImages ? R.layout.tale_item : R.layout.tale_item_small, TalesList, false);
@@ -44,12 +86,12 @@ public class ActivityColorings extends ActivityMain {
             tale.setColoringDetails(showBigImages);
 
             taleView.findViewById(R.id.play).setOnClickListener(v -> {
-                if (!this.isNetworkAvailable()){
-                    Toast.makeText(getActivity(), R.string.no_internet ,Toast.LENGTH_LONG).show();
+                if (!this.isNetworkAvailable()) {
+                    Toast.makeText(getActivity(), R.string.no_internet, Toast.LENGTH_LONG).show();
                     return;
                 }
 
-                if (ContextCompat.checkSelfPermission( this, Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(getActivity(), R.string.no_write_permission, Toast.LENGTH_LONG).show();
                     return;
                 }
@@ -64,13 +106,76 @@ public class ActivityColorings extends ActivityMain {
                         .show();
             });
         }
-
-        findViewById(R.id.nothingToShow).setVisibility(View.GONE);
-        findViewById(R.id.searchIcon).setVisibility(View.GONE);
-        findViewById(R.id.showFavorite).setVisibility(View.GONE);
     }
 
-    private void download(String url, String fileName){
+    private void hideSearch() {
+        searchIcon.setVisibility(View.VISIBLE);
+        searchField.setVisibility(View.GONE);
+
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .hideSoftInputFromWindow(searchField.getWindowToken(), 0);
+    }
+
+    private View.OnClickListener search = v -> {
+        searchIcon.setVisibility(View.GONE);
+        searchField.setVisibility(View.VISIBLE);
+        searchField.requestFocus();
+
+        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
+                .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    };
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void addSearchField() {
+        searchIcon = findViewById(R.id.searchIcon);
+        searchField = findViewById(R.id.searchField);
+
+        findViewById(R.id.toolbar).setOnClickListener(search);
+        searchIcon.setOnClickListener(search);
+
+        searchField.setText(Tales.getFilter());
+        searchField.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                Tales.setFilter(v.getText().toString());
+
+                hideSearch();
+                filterTales();
+                return true;
+            }
+            return false;
+        });
+
+        searchField.setOnTouchListener((view, event) -> {
+            int actionX = (int) event.getX();
+            int viewWidth = view.getWidth();
+            int buttonWidth = SettingsHelper.dpToPx(50);
+
+            if (viewWidth - buttonWidth <= actionX) {
+                searchField.setText("");
+                Tales.setFilter("");
+
+                hideSearch();
+                filterTales();
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (searchField.getVisibility() == View.VISIBLE && (event.getAction() == KeyEvent.ACTION_DOWN || event.getKeyCode() == KeyEvent.KEYCODE_BACK)) {
+            Tales.setFilter(searchField.getText().toString());
+            hideSearch();
+            filterTales();
+            return false;
+        }
+
+        return super.onKeyDown(keycode, event);
+    }
+
+    private void download(String url, String fileName) {
         Toast.makeText(getActivity(), R.string.coloring_download, Toast.LENGTH_LONG).show();
 
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
