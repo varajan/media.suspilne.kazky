330945d950cf92df80fb06029ed12bbc728badac
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2026-09-12 13:16:21 +0300

    Update category lists

--- app/src/main/java/media/suspilne/kazky/data/Categories.java
+++ app/src/main/java/media/suspilne/kazky/data/Categories.java
@@ -9,58 +9,54 @@ public class Categories {
     private static final Range[] ukrainianTales = {
             new Range(1, 2),
             new Range(6, 6),
-            new Range(9, 10),
-            new Range(17, 18),
-            new Range(19, 21),
-            new Range(23, 29),
-            new Range(31, 39),
-            new Range(41, 43),
-            new Range(45, 49),
-            new Range(51, 53),
-            new Range(55, 58),
-            new Range(60, 63),
-            new Range(66, 71),
-            new Range(73, 77),
-            new Range(81, 83),
-            new Range(86, 89),
-            new Range(91, 91),
-            new Range(95, 98),
-            new Range(101, 107),
-            new Range(109, 110),
-            new Range(113, 117)
-        };
+            new Range(12, 12),
+            new Range(17, 29),
+            new Range(31, 77),
+            new Range(104, 104),
+    };
 
     private static final Range[] internationalTales = {
-            new Range(3, 5),
-            new Range(7, 8),
-            new Range(11, 16),
-            new Range(22, 22),
-            new Range(30, 30),
-            new Range(40, 40),
-            new Range(44, 44),
-            new Range(50, 50),
-            new Range(54, 54),
-            new Range(59, 59),
-            new Range(64, 65),
-            new Range(72, 72),
-            new Range(78, 80),
-            new Range(84, 85),
-            new Range(90, 90),
-            new Range(92, 94),
-            new Range(99, 100),
-            new Range(108, 108),
-            new Range(111, 112),
-            new Range(123, 123)
-        };
+            new Range(3, 3),
+            new Range(7, 7),
+            new Range(13, 16),
+            new Range(78, 103),
+            new Range(105, 117),
+    };
 
     private static final Range[] bigWorldAnimalsTales = {
+            new Range(1, 6),
+            new Range(8, 13),
+            new Range(20, 23),
+            new Range(26, 27),
+            new Range(30, 30),
+            new Range(33, 35),
+            new Range(37, 37),
+            new Range(39, 40),
+            new Range(44, 44),
+            new Range(47, 47),
+            new Range(58, 59),
+            new Range(61, 61),
+            new Range(63, 63),
+            new Range(65, 65),
+            new Range(67, 67),
+            new Range(72, 72),
+            new Range(74, 76),
+            new Range(78, 78),
+            new Range(80, 82),
+            new Range(94, 94),
+            new Range(96, 96),
+            new Range(99, 100),
+            new Range(105, 105),
+            new Range(115, 115),
+            new Range(117, 117),
             new Range(124, 129),
-            new Range(142, 142)
-        };
+            new Range(142, 142),
+    };
 
     private static final Range[] modernAuthorTales = {
             new Range(118, 118),
-            new Range(142, 142)
+            new Range(123, 123),
+            new Range(125, 125),
         };
 
     public static List<Category> Items = Arrays.asList(
